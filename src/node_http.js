1cc6655c472cd857824b95c685141e5e8e796362
Kauan Reis <kauanpedroreis4@gmail.com>
2023-07-04 01:03:39 -0300

    todos, http nativo

--- src/node_http.js
+++ src/node_http.js
@@ -1,14 +1,9 @@
-const http =require('http')
+const http = require('http')
 
-
-const wait = (time) =>
-  new Promise(resolve =>
-    setTimeout(resolve, time)
-    )
-
+const wait = (time) => new Promise((resolve) => setTimeout(resolve, time))
 const todosDatabase = (() => {
-    let idSequence = 1
-    const todos = {}
+  let idSequence = 1
+  const todos = {}
 
   const insert = async (todo) => {
     await wait(500)
@@ -47,68 +42,64 @@ const todosDatabase = (() => {
     update,
     del,
   }
-
 })()
 
-const server =http.createServer((request,response)=>{
-  const jsonHeader={'Content-Type':'application/json'}
-//END-POINTs
+const server = http.createServer((request, response) => {
+  const jsonHeader = { 'Content-Type': 'application/json' }
+  // END-POINTs
 
-    /* GET /hello/name */
-  if(request.method==='GET' && /^\/hello\/\w+$/.test(request.url)){
-    const[,hello,name]=request.url.split('/')
+  /* GET /hello/name */
+  if (request.method === 'GET' && /^\/hello\/\w+$/.test(request.url)) {
+    const [, , name] = request.url.split('/')
     response.writeHead(200)
     response.end(`Hello ${name}!`)
     return
   }
-    /* GET /hello */
-  if(request.method === 'GET'&& request.url.startsWith('/hello')){
+  /* GET /hello */
+  if (request.method === 'GET' && request.url.startsWith('/hello')) {
     response.writeHead(200)
-    response.end("Hello Word!")
+    response.end('Hello Word!')
     return
   }
-   /* POST /echo */
-  if(request.method==='POST' && request.url.startsWith('/echo')){
+  /* POST /echo */
+  if (request.method === 'POST' && request.url.startsWith('/echo')) {
     response.writeHead(200)
     request.pipe(response)
     return
   }
-  //************* */
-  //** API TODOS** */
+  //* ************ */
+  //* * API TODOS** */
 
+  // POST /todos = criar
+  if (request.method === 'POST' && request.url.startsWith('/todos')) {
+    let bodyRaw = ''
 
+    request.on('data', (data) => (bodyRaw = data + bodyRaw))
 
-     //POST /todos = criar
-  if(request.method==='POST'&&request.url.startsWith('/todos')){
-    let bodyRaw =''
+    request.once('end', () => {
+      const todo = JSON.parse(bodyRaw)
 
-    request.on('data',data=> bodyRaw = data+bodyRaw)
-
-    request.once('end', ()=>{
-      const todo= JSON.parse(bodyRaw)
-
-      todosDatabase.insert(todo)
-      .then(inserted=>{
-        response.writeHead(201,jsonHeader)
+      todosDatabase.insert(todo).then((inserted) => {
+        response.writeHead(201, jsonHeader)
         response.end(JSON.stringify(inserted))
       })
     })
     return
   }
 
-  //PUT /todos/:id = Atualiar
-  if(request.method==="PUT"&& /^\/todos\/\d+$/.test(request.url)){
-    let bodyRaw=''
+  // PUT /todos/:id = Atualiar
+  if (request.method === 'PUT' && /^\/todos\/\d+$/.test(request.url)) {
+    let bodyRaw = ''
 
-    const[,todos,idRaw]=request.url.split('/')
+    const [, , idRaw] = request.url.split('/')
     const id = parseInt(idRaw)
 
-    request.on('data',data=>bodyRaw+=data)
+    request.on('data', (data) => (bodyRaw += data))
 
-    request.once('end', ()=>{
-      const todo = {...JSON.parse(bodyRaw),id}
+    request.once('end', () => {
+      const todo = { ...JSON.parse(bodyRaw), id }
 
-      todosDatabase.update(todo).then((update)=>{
+      todosDatabase.update(todo).then((update) => {
         response.writeHead(200, jsonHeader)
         response.end(JSON.stringify(update))
       })
@@ -116,49 +107,45 @@ const server =http.createServer((request,response)=>{
     return
   }
 
-    //GET /todos/:id = buscar
-  if(request.method==='GET'&&/^\/todos\/\d+$/.test(request.url)){
-    const[,todos,idRaw]=request.url.split('/')
+  // GET /todos/:id = buscar
+  if (request.method === 'GET' && /^\/todos\/\d+$/.test(request.url)) {
+    const [, , idRaw] = request.url.split('/')
     const id = parseInt(idRaw)
 
-    todosDatabase.get(id).then(listId=>{
-      if(!listId){
-        response.writeHead(404,jsonHeader)
-        response.end({message:"Resource not found"})
-      } else{
-        response.writeHead(200,jsonHeader)
+    todosDatabase.get(id).then((listId) => {
+      if (!listId) {
+        response.writeHead(404, jsonHeader)
+        response.end({ message: 'Resource not found' })
+      } else {
+        response.writeHead(200, jsonHeader)
         response.end(JSON.stringify(listId))
       }
-
     })
     return
   }
-    //GET /todos = listar
-  if(request.method==='GET'&&request.url.startsWith('/todos')){
-    todosDatabase
-    .list()
-    .then(todos=>{
-      response.writeHead(200,jsonHeader)
+  // GET /todos = listar
+  if (request.method === 'GET' && request.url.startsWith('/todos')) {
+    todosDatabase.list().then((todos) => {
+      response.writeHead(200, jsonHeader)
       response.end(JSON.stringify(todos))
     })
     return
   }
-    //DELETE /todos/:id = Deletar
-  if(request.method==="DELETE"&&/^\/todos\/\w+$/.test(request.url)){
-    const[,todos,id]=request.url.split('/')
+  // DELETE /todos/:id = Deletar
+  if (request.method === 'DELETE' && /^\/todos\/\w+$/.test(request.url)) {
+    const [, , id] = request.url.split('/')
 
-    todosDatabase.del(id).then((todos)=>{
-      response.writeHead(204,jsonHeader)
+    todosDatabase.del(id).then((todos) => {
+      response.writeHead(204, jsonHeader)
       response.end(JSON.stringify(todos))
     })
     return
   }
 
   response.writeHead(404)
-  response.end("Resource not found! 💀")
-
+  response.end('Resource not found! 💀')
 })
 
-server.listen(3000, '0.0.0.0',()=>{
+server.listen(3000, '0.0.0.0', () => {
   console.log('👾 server Started, listening on port 3000')
 })
