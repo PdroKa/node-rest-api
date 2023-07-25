6347ba59835f69d544e3b784e62b0f09cf2b367b
Kauan Reis <kauanpedroreis4@gmail.com>
2023-07-25 18:32:28 -0300

    feat: addded repository in memory + logica nos handlers de user

--- src/routes/users/route.js
+++ src/routes/users/route.js
@@ -1,5 +1,15 @@
 const { Router } = require('express')
 const withAsyncErrorHandler = require('../../middlewares/async-error')
+const { UsersRepository } = require('./repository')
+
+const router = Router()
+
+const repository = UsersRepository()
+
+const NotFound = {
+  error: 'Not found',
+  message: 'Resource not found',
+}
 
 /*
   CRUD de usuários
@@ -9,23 +19,41 @@ const withAsyncErrorHandler = require('../../middlewares/async-error')
   - D: delete
 */
 
-const router = Router()
-
 // ************
 // ** CREATE **
 // ************
 
 const createUser = async (req, res) => {
-  res.status(201).header('Location', '/users/IDENTIFICADOR')
+  // e se não for um JSON de usuário válido ?
+  const user = req.body
+
+  const inserted = await repository.insert(user)
+  const location = `/api/users/${inserted.id}`
+  res.status(201).header('Location', location).send(inserted)
 }
-router.post('/users', withAsyncErrorHandler(createUser))
+
+router.post('/', withAsyncErrorHandler(createUser))
 
 // ************
 // ** UPDATE **
 // ************
 
 const updateUser = async (req, res) => {
-  res.status(200).send({ user: 'ID Atualizado' })
+  // e se for NaN ?
+  const id = parseInt(req.params.id)
+
+  // e se não for um JSON de usuário válido ?
+  const body = req.body
+
+  // repetido
+  const registered = await repository.get(id)
+  if (!registered) {
+    return res.status(404).send(NotFound)
+  }
+
+  const user = { ...body, id }
+  const updated = await repository.update(user)
+  res.status(200).send(updated)
 }
 
 router.put('/:id', withAsyncErrorHandler(updateUser))
@@ -35,24 +63,42 @@ router.put('/:id', withAsyncErrorHandler(updateUser))
 // ************
 
 const deleteUser = async (req, res) => {
-  res.status(204).send({ user: 'ID Deletado' })
+  // e se for NaN ?
+  const id = parseInt(req.params.id)
+
+  // repetido
+  const registered = await repository.get(id)
+  if (!registered) {
+    return res.status(404).send(NotFound)
+  }
+
+  await repository.del(id)
+  res.status(204).send({ message: 'Deleted' })
 }
 
 router.delete('/:id', withAsyncErrorHandler(deleteUser))
 
-// ************
+// **********
 // ** READ **
-// ************
+// **********
 
-const listUser = async (_req, res) => {
-  res.status(200).send({ users: 'ARRAY DE USUARIOS' })
-}
+const listUsers = async (_req, res) =>
+  repository.list().then((users) => res.status(200).send({ users }))
 
 const getUser = async (req, res) => {
-  res.status(200).send({ users: 'Usuario do ID' })
+  // e se for NaN
+  const id = parseInt(req.params.id)
+
+  // repetido
+  const user = await repository.get(id)
+  if (!user) {
+    return res.status(404).send(NotFound)
+  }
+
+  res.status(200).send(user)
 }
 
-router.get('/', withAsyncErrorHandler(listUser))
+router.get('/', withAsyncErrorHandler(listUsers))
 router.get('/:id', withAsyncErrorHandler(getUser))
 
 module.exports = router
